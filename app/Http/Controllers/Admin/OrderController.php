e20971c75289b1ff5528184f1053f6ae02c9ee07
Qaweeds <qaweeds@gmail.com>
2021-09-20 01:05:48 +0300

    23, 25, 26

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        $orders = Order::with('status')->paginate(10);
+
+        return view('admin.orders.index', compact('orders'));
+    }
+
+    public function show(Order $order)
+    {
+        return view('admin.orders.show', compact('order'));
+    }
+}
