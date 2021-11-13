003eec3cf1c09ae3b94dff97eef98e32e6222224
Qaweeds <qaweeds@gmail.com>
2021-11-13 16:40:10 +0200

    dev

--- app/Repo/InvoiceRepository.php
+++ app/Repo/InvoiceRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repo;
 
 use App\Models\Order;
+use App\Models\OrderStatus;
 use App\Repo\Contracts\InvoiceRepositoryInterface;
 use LaravelDaily\Invoices\Invoice;
 
@@ -22,14 +23,21 @@ class InvoiceRepository implements InvoiceRepositoryInterface
                 ->pricePerUnit($product->pivot->single_price)
                 ->quantity($product->pivot->quantity);
         }
-        return Invoice::make()
+        $invoice = Invoice::make()
             ->name('Этот шаблон нужно хорошенько настраивать :)')
             ->buyer($customer)
+            ->status($order->status->name)
             ->series(__('Order'))
             ->sequence($order->id)
             ->addItems($items)
             ->notes(__('Thank you for purchase'))
             ->filename(now()->timestamp . '_' . $order->id . '_' . $order->user->surname)
             ->logo(public_path('vendor/invoices/sample-logo.png'));
+        if ($order->status->id == OrderStatus::query()->where('name', config('constants.db.order_statuses.in_process'))->value('id')) {
+            $invoice->payUntilDays('7');
+        }else{
+            $invoice->pay_until_days = false;
+        }
+        return $invoice;
     }
 }
