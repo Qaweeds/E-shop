50fc69f0261df07d427cdf73cc95aee20ac395ac
Qaweeds <qaweeds@gmail.com>
2021-11-13 14:27:36 +0200

    dev

--- app/Http/Middleware/TrustHosts.php
+++ app/Http/Middleware/TrustHosts.php
@@ -14,6 +14,7 @@ class TrustHosts extends Middleware
     public function hosts()
     {
         return [
+            'qaweeds-shop.herokuapp.com/',
             $this->allSubdomainsOfApplicationUrl(),
         ];
     }
